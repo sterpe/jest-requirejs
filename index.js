a2b644f591e0feb9109697a2ae6709eeb4e2373f
sterpe <srterpe@gmail.com>
2015-10-18 01:10:34 -0700

    relative paths, simple baseUrl paths

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
 "use strict";
 
+const esprima = require('esprima');
+const estraverse = require('estraverse');
+const escodegen = require('escodegen');
+const path = require('path');
+
 module.exports = function (config) {
 
-	return;
+	const requirejs = require('./lib/requirejs')(config.mainjs);
+
+	return {
+		process: function (src, filename) {
+			// deamdify the src if necessary
+			let t = require('./lib/deamdify')(src, { file: filename });
+			if (t === src) {
+				// just return it, it wasn't a require module to begin with.
+				return src;
+			}
+			const ast = esprima.parse(t, {
+			});
+
+			estraverse.replace(ast, {
+				enter: function (node) {
+				}
+				, leave: function (node) {
+					if (node.type === "CallExpression" &&
+							node.callee.type === "Identifier" &&
+							node.callee.name === "require") {
+						if (node.arguments[0].type === "Literal") {
+							// relative paths
+							// we just convert them to absolute paths...
+							if (/^\..*$/.test(node.arguments[0].value)) {
+								node.arguments[0].value = path.resolve(path.dirname(filename),
+									node.arguments[0].value);
+								return node;
+							}
+						}
+						if (node.arguments[0].value.indexOf("/") === -1 && (!/\.js$/.test(node.arguments[0].value))) {
+							node.arguments[0].value = path.resolve(requirejs.dirname
+								, requirejs.config.baseUrl
+								, node.arguments[0].value
+							);
+							return node;
+						}
+					}
+				}
+			});
+
+			return escodegen.generate(ast);
+		}
+	};
 }
